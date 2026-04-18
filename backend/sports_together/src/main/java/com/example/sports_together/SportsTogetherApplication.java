25dc94e93037bddcb5f075a121879229a8dc81cf
larissa.aboud <larissa.abboud@lau.edu>
2026-04-18 23:22:12 +0200

    initial backend start

---
+++ backend/sports_together/src/main/java/com/example/sports_together/SportsTogetherApplication.java
@@ -0,0 +1,13 @@
+package com.example.sports_together;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsTogetherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportsTogetherApplication.class, args);
+	}
+
+}
---
+++ backend/sports_together/src/test/java/com/example/sports_together/SportsTogetherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sports_together;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportsTogetherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
